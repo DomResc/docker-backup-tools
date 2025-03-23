834cb1d04c82b601e3a7a3eb4ddf4ebd4a2352ec
Domenico Rescigno <domenico.rescigno@gmail.com>
2025-03-23 16:02:15 +0100

    Fixed ownership inconsistency

--- docker_backup.sh
+++ docker_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Docker Volume Backup - A utility to backup Docker volumes
-# https://github.com/yourusername/docker-volume-tools
+# https://github.com/domresc/docker-volume-tools
 # 
 # MIT License
 # 
-# Copyright (c) 2025 Your Name
+# Copyright (c) 2025 Domenico Rescigno
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
